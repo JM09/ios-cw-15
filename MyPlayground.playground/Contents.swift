d7f83fb07f5356886b9fd4c161ecc342be3fa087
JM09 <jassimbackup62@gmail.com>
2022-03-19 10:39:15 +0300

    done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Student {
+    var name : String
+var age : Int
+var grade : Double
+var attendence : Bool
+}
+    var Jassem = Student(name: "jassem" , age: 16 , grade: 90 , attendence: true)
+
